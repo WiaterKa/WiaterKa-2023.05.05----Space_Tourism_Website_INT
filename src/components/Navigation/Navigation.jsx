183a224850e75fb74a14e420bb1dacf8f2aaa1ab
WiaterKa <kamilwiater.uj@gmail.com>
2023-05-05 20:19:37 +0200

    Burger_Finished

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,15 +4,22 @@ import Home from "../../links/Home/Home";
 import Destination from "../../links/Destination/Destination";
 import Crew from "../../links/Crew/Crew";
 import Technology from "../../links/Technology/Technology";
+import { useState } from "react";
 
 export default function Navigation() {
+  const [isActiveBurg, setsActiveBurg] = useState("");
+
+  const handleToggle = () => {
+    setsActiveBurg(!isActiveBurg);
+  };
+
   return (
     <nav>
       <div className="logo">
         <img src="public/shared/logo.svg" alt="logo" />
         <div className="logo-line"></div>
       </div>
-      <div className="nav-items">
+      <div className={`nav-items ${isActiveBurg ? "active" : ""}`}>
         <Link to="/" element={<Home />}>
           <span>00</span> Home
         </Link>
@@ -26,6 +33,14 @@ export default function Navigation() {
           <span>03</span>Technology
         </Link>
       </div>
+      <button
+        className={`burger ${isActiveBurg ? "active" : ""}`}
+        onClick={handleToggle}
+      >
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </button>
     </nav>
   );
 }
